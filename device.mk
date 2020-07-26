074fee27ab05f3cfeb591c9d2a6a520dc1141dcc
Swamoy14 <shubhasankar14@gmail.com>
2020-07-26 20:39:36 +0530

    X00T: Enabled overlay system

--- device.mk
+++ device.mk
@@ -242,7 +242,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-system
     
 # Overlays - Enforce RRO
 PRODUCT_ENFORCE_RRO_TARGETS := *
